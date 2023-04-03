d1382edfab29d329a18f7dc425b00a35d3a89ebf
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-04-03 22:18:36 +0300

    Add addTodos operation to click event

--- src/App.js
+++ src/App.js
@@ -3,21 +3,34 @@ import { useState } from "react";
 const App = () => {
 
   const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if(todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
 
   return (
     <div className="App">
       <h1>React Todo App</h1>
       <div className="input-wrapper">
-        <input 
-          type="text" 
-          name="todo" 
-          placeholder="Create a new todo" 
-          value={todo} 
+        <input
+          type="text"
+          name="todo"
+          placeholder="Create a new todo"
+          value={todo}
           onChange={(event) => {
             setTodo(event.target.value);
           }}
         />
-        <button className="add-button">Add</button>
+        <button
+          className="add-button"
+          onClick={addTodo}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
