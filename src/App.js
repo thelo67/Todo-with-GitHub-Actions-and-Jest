bdc74f62523be8dd7a6bafb7ea01205c96406bb8
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-04-03 22:07:31 +0300

    Add basic mockup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <h1>React Todo App</h1>
+      <div className="input-wrapper">
+        <input type="text" name="todo" placeholder="Create a new todo" />
+        <button className="add-button">Add</button>
+      </div>
     </div>
   );
 }
