818c4a2273346b234c11bfdbb785f82a90ffaf13
Tuomo Helo <tuomo.helo@turkuamk.fi>
2023-04-03 22:11:46 +0300

    Add empty useState hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import { useState } from "react";
 
 const App = () => {
+
+  const [todo, setTodo] = useState("");
+
   return (
     <div className="App">
       <h1>React Todo App</h1>
       <div className="input-wrapper">
-        <input type="text" name="todo" placeholder="Create a new todo" />
+        <input 
+          type="text" 
+          name="todo" 
+          placeholder="Create a new todo" 
+          value={todo} 
+          onChange={(event) => {
+            setTodo(event.target.value);
+          }}
+        />
         <button className="add-button">Add</button>
       </div>
     </div>
   );
-}
+};
 
 export default App;
